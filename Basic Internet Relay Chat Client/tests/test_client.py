33165069d0c4e3eb95d11484ebfc91f037bc32ee
Haru <127700987+Hikariharu@users.noreply.github.com>
2023-11-01 23:32:50 -0400

    Update test_client.py

--- Basic Internet Relay Chat Client/tests/test_client.py
+++ Basic Internet Relay Chat Client/tests/test_client.py
@@ -1,10 +1,12 @@
 import pytest
 import time
 import threading
-from client import app, client_socket, messages, message_entry
+import tkinter as tk
+from client_logic import connect_to_server, send_message, receive_message
 
 @pytest.fixture
 def start_app():
+    app = tk.Tk()
     app.after_cancel(app._after)  # Stop the app's main loop
     t = threading.Thread(target=app.mainloop)  # Create a thread for the app
     t.daemon = True
@@ -14,18 +16,21 @@ def start_app():
     app.quit()
 
 def test_client_connection(start_app):
-    assert client_socket.connect(('localhost', 6667)) is None
+    host = 'localhost'
+    port = 6667
+    client_socket = connect_to_server(host, port)
+    assert client_socket is not None
     time.sleep(1)  # Wait for the connection
 
 def test_send_receive_message(start_app):
     message = "Test message"
+    message_entry = tk.Entry()
     message_entry.delete(0, 'end')  # Clear the message entry
     message_entry.insert(0, message)  # Set the test message
     time.sleep(1)  # Wait for the UI to update
-    message_entry.event_generate('<Return>')  # Simulate pressing the Return key
-    time.sleep(1)  # Wait for the message to be sent and received
-    text = messages.get('1.0', 'end-1c')  # Retrieve the content of the Text widget
-    assert message in text  # Check if the message is received
+    assert send_message(None, message) is None  # Simulate sending the message
+    received_message = receive_message(None)  # Simulate receiving a message
+    assert received_message == message  # Check if the received message matches
 
 def test_update_running_time(start_app):
     time.sleep(5)  # Wait for the running time to update
